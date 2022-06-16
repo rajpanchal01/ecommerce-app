d563ee04fed82781e9daa64d952b3289b3b19e27
rajpanchal01 <rajpanchal2810@gmail.com>
2022-06-16 18:51:37 +0530

    migration commit

--- db/schema.rb
+++ db/schema.rb
@@ -80,14 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_112159) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "product_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.string "total_amount"
     t.datetime "created_at", null: false
