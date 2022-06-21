afaac8402c7a739a6a7d854025a57a8a46cd1e1a
rajpanchal01 <rajpanchal2810@gmail.com>
2022-06-21 10:11:55 +0530

    change: type of gst_id and pan_id

--- db/migrate/20220620174327_create_sellers.rb
+++ db/migrate/20220620174327_create_sellers.rb
@@ -1,8 +1,8 @@
 class CreateSellers < ActiveRecord::Migration[7.0]
   def change
     create_table :sellers do |t|
-      t.bigint :gst_id, :null => false
-      t.bigint :pan_id,:null => false
+      t.string :gst_id, :null => false
+      t.string :pan_id,:null => false
       t.string :seller_location
 
       t.timestamps
